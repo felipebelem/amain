38dc1735347cfa8da35aab32ee15c3da7093898d
Felipe Belém <felipebelem@pucminas.br>
2026-02-28 16:58:40 -0300

    adding initial blank items

---
+++ templates/template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define ROUND(x) ((long long)((x) + 0.5))
+#define PI (acos(-1.0))
+#define EPS (1e-9)
+#define LOG(a,b) (1.0*log(a)/log(b))
+#define FAST_IO() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+
+void solution(void) {
+
+}
+
+int main(void) {
+    FAST_IO();
+
+}
